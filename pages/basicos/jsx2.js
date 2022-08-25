b3e01a57b56f6ad188c96842b8414724b9cadaf9
ronaldoroncode <roncodeit@gmail.com>
2022-08-24 22:15:20 -0300

    Primeiros testes com NextJS

---
+++ pages/basicos/jsx2.js
@@ -0,0 +1,14 @@
+export default function jsx2() {
+	const conteudo = (
+		<div>
+			<h1>JSX #02</h1>
+		</div>
+	)
+	const conteudo2 = (
+		<div>
+			<h1>Conteúdo 2</h1>
+		</div>
+	)
+	
+	return conteudo	
+}
---
+++ pages/basicos/jsx4.jsx
@@ -0,0 +1,11 @@
+export default function jsx4() {
+	const texto = " Tesla Motors"
+	const subtitulo = 3 + texto + texto + texto
+
+	return (
+		<div>
+			<h1>Integração JS e JSX</h1>
+			<h1>{subtitulo}</h1>
+		</div>
+	)
+}
